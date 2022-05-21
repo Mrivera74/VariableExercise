ca4ad086cbadcb3ca7b46040e4d1328f06c043f7
Mark Rivera <mrivera7475@gmail.com>
2022-05-20 23:51:13 -0400

    created a VS project for testing out Variables

---
+++ VSprojects/PlayingWithVariables/PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    namespace PlayingWithVariablesConsole
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            //Declared Variables
+            string name;
+            int age;
+            char correct;
+            bool NewCareer;
+            double length;
+            decimal height;
+
+            //Initialized Variables
+            name = "Mark Rivera";
+            age = 32;
+            correct = 'A';
+            NewCareer = true;
+            length = 76.2;
+            height = 24.2m;
+
+            Console.WriteLine($"{name} {age} Correct letter {correct} This a new career {NewCareer} How long is the table {length} How tall is the table {height}");
+
+        }
+    }
+}
+
+
+
