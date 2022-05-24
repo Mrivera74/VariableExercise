92a7d7cee345512023f6f704970c47b30f9b03ce
Mark Rivera <mrivera7475@gmail.com>
2022-05-24 16:00:39 -0400

    created file for Variable Exercise

---
+++ VSprojects/PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+    namespace PlayingWithVariablesConsole
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            //Declared Variables
+            string name;
+            int age;
+            char correct;
+            bool NewCareer;
+            double length;
+            decimal height;
+
+            //Initialized Variables
+            name = "Mark Rivera";
+            age = 32;
+            correct = 'A';
+            NewCareer = true;
+            length = 76.2;
+            height = 24.2m;
+
+            //Interpolate Variables
+            string dogName = "Rocky";
+            int dogAge = 10;
+            Char typeOfDog = 'a';
+            double dogLength = 24.5;
+            decimal dogweight = 40.65m;
+            bool purebreed = true;
+
+            Console.WriteLine($"{name} {age} Correct letter {correct} This a new career {NewCareer} How long is the table {length} How tall is the table {height}");
+            Console.WriteLine($"My dog's name is {dogName}. He is {dogAge}, his height is {dogLength} and his weight is {dogweight}. " +
+                $"It is {purebreed} that my dog is a purebreed. If your would like to see the dog pictures select a, if not select b.");
+                $")
+        }
+    }
+}
+
+
+
---
+++ VariableExercise/VariableExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿//Declared Variables
+string dogName;
+int dogAge;
+char dogPic;
+bool purebreed;
+double dogHeight;
+decimal dogWeight;
+
+//Initialized Variables
+dogName = "Chino";
+dogAge = 10;
+dogPic = 'a';
+purebreed = true;
+dogHeight = 24.3;
+dogWeight = 50.3m;
+
+Console.WriteLine($"My dog's name is {dogName}. He is {dogAge} years old, he is {dogHeight} inches tall and weights {dogWeight}lbs. It is {purebreed} that my dog is a purebreed. If your would like to see the dog pictures select a, if not select b. {dogPic}");
+
+//Interpolerize Variables
+string dogsName = "Chino";
+int dogsAge = 10;
+char dogsPic = 'a';
+bool breedOfDog = true;
+double dogsHeight = 24.3;
+decimal dogsWeight = 50.3m;
+
+Console.WriteLine($"My dog's name is {dogsName}. He is {dogsAge} years old, he is {dogsHeight} inches tall and weights {dogsWeight}lbs. It is {breedOfDog} that my dog is a purebreed. If your would like to see the dog pictures select a, if not select b. {dogsPic}");
+
